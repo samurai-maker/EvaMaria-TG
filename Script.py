4dff432578a2af623e5149c2f187e5bab0402536
Mukunda <83640801+mukundambbs@users.noreply.github.com>
2022-02-21 20:57:31 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,10 +1,10 @@
 class script(object):
     START_TXT = """<b>Hello Nanba/Nanbi {}👋
-My Name Is <a href=https://t.me/{}>{}</a>, I Can Provide Movies For only @LinkZz_MBBS,\n\n👨‍💻 Developer : <a href='https://t.me/mukunda3146'>≡ 𝗠𝗨𝗞𝗨𝗡𝗗𝗔 ≡</a></b>"""
+My Name Is <a href=https://t.me/{}>{}</a>, I Can Provide Movies For only @LinkZz_MBBS,\n\n👨‍💻 Developer : <a href='https://t.me/I_m_Kristy'>✯°• Iᴍ Kʀɪsᴛʏ கிறிஸ்டி Via @KaipullaVadiveluOffl  •°✯ « Tᴇᴀᴍ Kᴀɪᴘᴜʟʟᴀ​ »</a></b>"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/mukunda3146'>≡ 𝗠𝗨𝗞𝗨𝗡𝗗𝗔 ≡</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/I_m_Kristy'>✯°• Iᴍ Kʀɪsᴛʏ கிறிஸ்டி Via @KaipullaVadiveluOffl  •°✯ « Tᴇᴀᴍ Kᴀɪᴘᴜʟʟᴀ​ »</a></b>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: <a href='https://docs.pyrogram.com'>𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼</a>
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
